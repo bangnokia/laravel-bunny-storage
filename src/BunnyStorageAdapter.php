9ae768c7dbcae44a4f5076ae65c873902010775d
bangnokia <bangnokia@gmail.com>
2025-04-09 15:47:02 +0700

    add getUrl method to BunnyStorageAdapter for improved URL handling

--- src/BunnyStorageAdapter.php
+++ src/BunnyStorageAdapter.php
@@ -6,4 +6,8 @@ use PlatformCommunity\Flysystem\BunnyCDN\BunnyCDNAdapter;
 
 class BunnyStorageAdapter extends BunnyCDNAdapter
 {
+    public function getUrl(?string $path = null): string
+    {
+        return parent::getUrl((string) $path);
+    }
 }
